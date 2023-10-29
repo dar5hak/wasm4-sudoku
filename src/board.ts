de03e298973eabb073f6c8bfcae6b498c7ffbf5c
Darshak Parikh <darshak@proton.me>
2023-10-30 00:29:09 +0530

    Simplify cell width calculation

--- src/board.ts
+++ src/board.ts
@@ -4,8 +4,7 @@ import { Cell } from "./cell";
 
 const GRID_ORIGIN_X = 12;
 const GRID_ORIGIN_Y = 4;
-const CELL_INNER_WIDTH = 15;
-const CELL_OUTER_WIDTH = CELL_INNER_WIDTH + 1;
+const CELL_WIDTH = 15;
 
 export function drawGrid(): void {
   store<u16>(w4.DRAW_COLORS, 0x321);
@@ -18,8 +17,8 @@ export function drawActiveCell(activeCell: Cell): void {
   w4.rect(
     activeCellOrigin[0],
     activeCellOrigin[1],
-    CELL_OUTER_WIDTH,
-    CELL_OUTER_WIDTH,
+    CELL_WIDTH + 1,
+    CELL_WIDTH + 1,
   );
 }
 
@@ -27,8 +26,8 @@ export function getCellByPoint(x: i32, y: i32): Cell {
   const adjustedX = x - GRID_ORIGIN_X;
   const adjustedY = y - GRID_ORIGIN_Y;
 
-  const cellX: u8 = (adjustedX / CELL_INNER_WIDTH) as u8;
-  const cellY: u8 = (adjustedY / CELL_INNER_WIDTH) as u8;
+  const cellX: u8 = (adjustedX / CELL_WIDTH) as u8;
+  const cellY: u8 = (adjustedY / CELL_WIDTH) as u8;
 
   return new Cell(cellX, cellY);
 }
@@ -36,15 +35,15 @@ export function getCellByPoint(x: i32, y: i32): Cell {
 export function isOnGrid(x: i32, y: i32): boolean {
   return (
     x >= GRID_ORIGIN_X &&
-    x < GRID_ORIGIN_X + 9 * CELL_INNER_WIDTH &&
+    x < GRID_ORIGIN_X + 9 * CELL_WIDTH &&
     y >= GRID_ORIGIN_Y &&
-    y < GRID_ORIGIN_Y + 9 * CELL_INNER_WIDTH
+    y < GRID_ORIGIN_Y + 9 * CELL_WIDTH
   );
 }
 
 function getCellOrigin(cell: Cell): i32[] {
   return [
-    GRID_ORIGIN_X + CELL_INNER_WIDTH * cell.x,
-    GRID_ORIGIN_Y + CELL_INNER_WIDTH * cell.y,
+    GRID_ORIGIN_X + CELL_WIDTH * cell.x,
+    GRID_ORIGIN_Y + CELL_WIDTH * cell.y,
   ];
 }
