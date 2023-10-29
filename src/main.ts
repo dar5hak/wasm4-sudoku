423c6338b179b16c71f953c382080eef99476d51
Darshak Parikh <darshak@proton.me>
2023-10-29 19:29:23 +0530

    Move active cell

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,29 @@
 import { drawActiveCell, drawGrid } from "./board";
 import { State } from "./state";
+import * as w4 from "./wasm4";
+
+let previousGamepad: u8;
+let state: State;
 
 export function update(): void {
-  let state: State;
   if (!state) state = new State();
 
+  const gamepad = load<u8>(w4.GAMEPAD1);
+  const pressedThisFrame = gamepad & (gamepad ^ previousGamepad);
+  previousGamepad = gamepad;
+
+  handleInput(gamepad, pressedThisFrame);
+  draw(state);
+}
+
+function handleInput(gamepad: u8, pressedThisFrame: u8): void {
+  if (pressedThisFrame & w4.BUTTON_UP) state.moveUp();
+  if (pressedThisFrame & w4.BUTTON_DOWN) state.moveDown();
+  if (pressedThisFrame & w4.BUTTON_LEFT) state.moveLeft();
+  if (pressedThisFrame & w4.BUTTON_RIGHT) state.moveRight();
+}
+
+function draw(state: State): void {
   drawGrid();
   drawActiveCell(state.activeCell);
 }
--- src/state.ts
+++ src/state.ts
@@ -2,4 +2,24 @@ import { Cell } from "./cell";
 
 export class State {
   activeCell: Cell = new Cell(0, 0);
+
+  moveUp(): void {
+    if (this.activeCell.y == 0) this.activeCell.y = 8;
+    else this.activeCell.y--;
+  }
+
+  moveDown(): void {
+    if (this.activeCell.y == 8) this.activeCell.y = 0;
+    else this.activeCell.y++;
+  }
+
+  moveLeft(): void {
+    if (this.activeCell.x == 0) this.activeCell.x = 8;
+    else this.activeCell.x--;
+  }
+
+  moveRight(): void {
+    if (this.activeCell.x == 8) this.activeCell.x = 0;
+    else this.activeCell.x++;
+  }
 }
